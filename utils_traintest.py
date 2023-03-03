fb40c1bc6c2b9d96c4a45201d374ea8f20d46a46
unknown <rantoringagain@gmail.com>
2023-03-03 15:50:12 +0900

    Trainer class is ongoing

--- utils_traintest.py
+++ utils_traintest.py
@@ -1,13 +1,17 @@
-from dataloader import DigitDataset
-from utils import reshape, drawimg, tensor_to_pltimg, multidrawimg, train, test
+import os
+import csv
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+
 import torch
 from torch import nn
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
-import os
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-import csv
+
+from dataloader import DigitDataset
+import utils as ut
+
 
 def train(dataloader: DataLoader, model, loss_fn, optimizer, device):
     size = len(dataloader.dataset)
@@ -45,10 +49,10 @@ def test(dataloader, model, loss_fn, device, valid=True):
             print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
         elif valid == False:
             counter = 0
-            filename = f"Submission_{counter}.csv"
+            filename = f"Submissions/Submission_{counter}.csv"
             while os.path.exists(filename):
                 counter += 1
-                filename = f"Submission_{counter}.csv"
+                filename = f"Submissions/Submission_{counter}.csv"
             with open(filename, 'a+', newline='') as f:  # 'a+' means append to a file
                 thewriter = csv.writer(f)
                 for X in dataloader:
